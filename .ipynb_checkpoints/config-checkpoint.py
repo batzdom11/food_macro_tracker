c4406ade87c94919fd439fd95dfea662afce01c4
Dominik Batz <dominik.batz@nzz.ch>
2025-03-19 22:23:30 +0100

    base api url config

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,3 +1,3 @@
-#BASE_API_URL = "https://food-macro-tracker.onrender.com"
-BASE_API_URL = "http://127.0.0.1:8000"
+BASE_API_URL = "https://food-macro-tracker.onrender.com"
+#BASE_API_URL = "http://127.0.0.1:8000"
 API_URL = "https://food-macro-tracker.onrender.com"
